1a4604875768aff10cb4edd51b83ca9a7943d6e9
yoshi-koyama <koyama@reytech.co.jp>
2023-09-09 20:45:57 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/com/koyama/namemanagement/NamemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.koyama.namemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koyama/namemanagement/NamemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koyama.namemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
